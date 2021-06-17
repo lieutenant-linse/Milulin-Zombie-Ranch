cbbe02b4ae4351f95b397ec1cf1c0e61f4eff5b0
Linus Stetter <stetter.linus@gmail.com>
2021-06-17 16:57:44 +0200

    new Player Movements and Animations

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+        private float moveSpeed = 5f;
+
+        public Rigidbody2D rb;
+
+
+    Vector2 movement;
+
+
+
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
