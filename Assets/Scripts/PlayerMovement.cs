2199bac38c4f3f06b33fe4aaf86848e3bd086b38
Ludwig Degenhardt <ludwwig.degenhardt@lmu.de>
2021-06-20 17:04:49 +0200

    Heutige Änderungen: Walk und Idle Animations in 8 Richtungen

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{
-    [SerializeField]
-        private float moveSpeed = 5f;
+{   
+    public float moveSpeed = 5f;
 
-        public Rigidbody2D rb;
+    public Rigidbody2D rb;
 
+
+    public Animator animator;
 
     Vector2 movement;
+
 
 
 
@@ -19,10 +21,17 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
     }
 }
