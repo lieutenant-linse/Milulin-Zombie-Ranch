adc0c98fb6cf1e2ce18729077dafa26b9d7a2935
Michelle Zender <ntb@MacBook-Pro.fritz.box>
2021-06-21 13:51:33 +0200

    idle direction

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -22,8 +22,11 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+        if (movement != Vector2.zero) { 
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
+        }
+
         animator.SetFloat("Speed", movement.sqrMagnitude);
 
 
