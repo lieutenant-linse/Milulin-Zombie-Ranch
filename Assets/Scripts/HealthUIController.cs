01cfb38737062187034cfa43239df879537556d4
Linus Stetter <stetter.linus@gmail.com>
2021-07-17 22:25:24 +0200

    more comments

--- Assets/Scripts/HealthUIController.cs
+++ Assets/Scripts/HealthUIController.cs
@@ -7,13 +7,14 @@ public class HealthUIController : MonoBehaviour
 {
 
     public GameObject heartContainer;
-    private float fillValue;
+    private float fillValue; //for the Heart UI Container
 
     // Update is called once per frame
     void Update()
     {
+        //univerally applicable Health Controller
         fillValue = (float)GameController.Health;
-        fillValue = fillValue / GameController.MaxHealth;
+        fillValue = fillValue / GameController.MaxHealth; //curently: 3/3 - Means one hit equal to one heart
         heartContainer.GetComponent<Image>().fillAmount = fillValue;
     }
 }
